1f0734218d2abcae22794642a0eb12eabd76f027
Terry S <itsterry@gmail.com>
2025-02-14 12:46:53 +0000

    db-tweak

--- db/production.sql
+++ db/production.sql
@@ -1,3 +1,9 @@
-CREATE DATABASE xczaro_production;
 CREATE USER 'xczaro'@'%' IDENTIFIED BY 'poiuyqwert';
+CREATE DATABASE xczaro_production;
 GRANT ALL PRIVILEGES ON xczaro_production.* TO 'xczaro'@'%';
+CREATE DATABASE xczaro_production_cache;
+GRANT ALL PRIVILEGES ON xczaro_production_cache.* TO 'xczaro'@'%';
+CREATE DATABASE xczaro_production_queue;
+GRANT ALL PRIVILEGES ON xczaro_production_queue.* TO 'xczaro'@'%';
+CREATE DATABASE xczaro_production_cable;
+GRANT ALL PRIVILEGES ON xczaro_production_cable.* TO 'xczaro'@'%';
